175cda0f4362003a1525a306d835dffcb7075f58
kennytrionfo <kentrionfo@go@gmail.com>
2014-05-22 15:51:44 -0600

    adding number file

---
+++ number.rb
@@ -0,0 +1,9 @@
+puts "please give me any number"
+number1 = gets.chomp.to_i
+
+puts "now give me another number. any number"
+number2 = gets.chomp.to_i
+
+result = number1 + number2
+
+puts "Great, so those numbers together equal #{result}."
