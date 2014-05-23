4e3dec59248ec31b96a4140396ee57dfa15ea49a
kennytrionfo <kentrionfo@go@gmail.com>
2014-05-23 14:31:22 -0600

    1st

--- number.rb
+++ number.rb
@@ -1,9 +1,28 @@
-puts "please give me any number"
+puts "Please give me any number"
 number1 = gets.chomp.to_i
 
-puts "now give me another number. any number"
+puts "Now give me another number, any number"
 number2 = gets.chomp.to_i
 
 result = number1 + number2
 
-puts "Great, so those numbers together equal #{result}."
+puts "\nGreat, so those numbers together equal #{result}.\n"
+puts "But maybe you're sick of adding.\n So other than addition, out of subtraction, division and multiplication...which one would YOU like to perform with these two numbers?"
+
+math_type = gets.chomp
+if math_type == "subtraction"
+  subtraction_result = number1 - number2
+  puts "So #{number1} - #{number2} equals #{subtraction_result}."
+elsif math_type == "division"
+  division_result = number1 / number2
+  puts "So #{number1} / #{number2} equals #{divsion_result}."
+elsif math_type == "multiplication"
+  multiplication_result = number1 * number2
+  puts "So #{number1} * #{number2} equals #{multiplication_result}."
+end
+
+
+case math_type
+when "subtraction"
+  "So #{number1} - #{number2} equals #{number1 - number}"
+  
